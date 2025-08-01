46667c2592db07f1f2ef0ea0dd4b39da9909aca4
Deanna Sharma <83244002+DeannaSharma@users.noreply.github.com>
2025-08-01 14:02:31 +0100

    fix: update worksLocationCoordinates column to be text (#149)

---
+++ services/migrations/1754046609-works_location_coord_text.mjs
@@ -0,0 +1,15 @@
+import { sql } from "kysely";
+
+/**
+ * @param db {Kysely<any>}
+ */
+export async function up(db) {
+    await sql`ALTER TABLE roadworks MODIFY worksLocationCoordinates TEXT`.execute(db);
+}
+
+/**
+ * @param db {Kysely<any>}
+ */
+export async function down(db) {
+    await sql`ALTER TABLE roadworks MODIFY worksLocationCoordinates VARCHAR(10000)`.execute(db);
+}
